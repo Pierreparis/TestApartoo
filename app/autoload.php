19c673056b9d109432eedf18e657cffef9f7fcbf
Pierre <pierrelab2014@gmail.com>
2015-07-31 17:16:34 +0200

    conf fos

--- app/autoload.php
+++ app/autoload.php
@@ -7,7 +7,10 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
+$loader->registerNamespaces(array(
+    // ...
+    'FOS' => __DIR__.'/../vendor/bundles',
+));
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
