8ef0e17d32416ff98d67827bd2259cc210734900
Pierre <pierrelab2014@gmail.com>
2015-07-31 12:55:23 +0200

    hello world

---
+++ src/OC/PlatformBundle/Resources/config/routing.php
@@ -0,0 +1,14 @@
+<?php
+
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+$collection = new RouteCollection();
+
+$collection->add('oc_platform_homepage', new Route('/hello/{name}', array(
+    '_controller' => 'OCPlatformBundle:Default:index',
+)));
+$collection->add('hello_the_world', new Route('/hello-world', array(
+    '_controller' => 'OCPlatformBundle:Advert:index',
+)));
+return $collection;
