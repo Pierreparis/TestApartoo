6affcf30b0d1ca76760f47b202e884bff81e121d
Pierre <pierrelab2014@gmail.com>
2015-08-01 16:25:50 +0200

    test templ

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,13 +3,21 @@
 // src/OC/PlatformBundle/Controller/AdvertController.php
 
 namespace OC\PlatformBundle\Controller;
-
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-class AdvertController
+class AdvertController extends Controller
 {
     public function indexAction()
     {
         return new Response("Hello World jkjk!");
     }
+  public function viewAction()
+  {
+	 $id=4848;
+    return $this->render(
+      'OCPlatformBundle:Advert:view.html.twig',
+      array('id'  => $id)
+    );
+  }
 }
--- src/OC/PlatformBundle/Resources/config/routing.php
+++ src/OC/PlatformBundle/Resources/config/routing.php
@@ -8,6 +8,9 @@ $collection = new RouteCollection();
 $collection->add('oc_platform_homepage', new Route('/hello/{name}', array(
     '_controller' => 'OCPlatformBundle:Default:index',
 )));
+$collection->add('wah', new Route('/wa', array(
+    '_controller' => 'OCPlatformBundle:Advert:view',
+)));
 $collection->add('hello_the_world', new Route('/hello-world', array(
     '_controller' => 'OCPlatformBundle:Advert:index',
 )));
